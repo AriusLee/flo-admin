75ec8f6bbf5e908312bfb8d9e0f98d0675b38cf2
Arius <ariusleechihin@gmail.com>
2012-08-24 12:18:42 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FloAdmin::Application.config.secret_token = '4d4fe9e292938abe143536503e1171c8c000637f9b6bd18a2fe96229e10a8cbc84375184790dd566a82ebb73a15206dbb4fe10438a43135f3301a7163a187748'
